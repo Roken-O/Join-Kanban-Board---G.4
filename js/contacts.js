edc5007e3850c462eca4a599679d85f50b4be4e6
BuenyaminTig <buenyamin.tig@gmail.com>
2024-06-21 09:19:28 +0200

    contacts css responsive (in part)

--- js/contacts.js
+++ js/contacts.js
@@ -89,6 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function init() {
   document.getElementById("add-new-contact-popUp-bg").classList.add("d-none");
   document.getElementById("edit-contact-popUp-bg").classList.add("d-none");
+  document.getElementById("PopUp-edit-delete-bg").classList.add("display-none");
 }
 
 
@@ -115,7 +116,7 @@ function editContactPopUp() {
     .classList.add("add-new-or-edit-contact-popUp-bg");
 }
 
-function showContact(){
+function showContactInfo(){
   document.getElementById("contact").classList.add("show-contact");
 }
 
@@ -124,10 +125,21 @@ function showContactResponsive (){
   if (width <= 1050) {
     document.getElementById("contacts-list").classList.add("display-none");
     document.getElementById("contacts-container-responsive").classList.add("d-unset");
+    document.getElementById("back-icon").classList.add("d-unset");
+
   }
 }
 
 function closeContact(){
   document.getElementById("contacts-list").classList.remove("display-none");
   document.getElementById("contacts-container-responsive").classList.remove("d-unset");
+  document.getElementById("back-icon").classList.remove("d-unset");
+}
+
+function closePopUpEditDelete(){
+  document.getElementById("PopUp-edit-delete-bg").classList.add("display-none");
+}
+
+function openPopUpEditDelete(){
+  document.getElementById("PopUp-edit-delete-bg").classList.remove("display-none");
 }
