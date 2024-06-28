4cec2dded58705f953d4a7b5699815965bdbd6c4
Felix Kroth <felix_kroth@web.de>
2024-06-28 13:50:01 +0200

    added edit function to board detail popup / added categroy and priority function to board detail popup

--- js/board.js
+++ js/board.js
@@ -9,7 +9,6 @@ let currentDraggedElement;
 // }, 10);
 // }
 
-
 function openTaskDetails(taskId) {
   let taskDetails = document.getElementById('task');
   taskDetails.style.display = 'flex';
@@ -48,9 +47,10 @@ function openTaskDetails(taskId) {
           <span class="popup-span">Priority:</span>
           <div class="popup-priority-img-container">${task['taskPriorityText']} <img src="${task['taskPriority']}"></div>
         </div>
-        <div >
+        <div id="contacts-list-popup">
           <span class="popup-span">Assigned To:</span>
           ${assignedContacts}
+          
         </div>
         <div class="popup-subtasks-container">
           <span class="popup-span">Subtasks</span>
@@ -59,11 +59,11 @@ function openTaskDetails(taskId) {
         <div class="popup-modify-delete-container">
           <div class="popup-del-edit">
             <img src="/assets/img/delete_icon.png">
-            <span onclick="deleteTask('${task['taskId']}');  closePopup()"> Delete </span>
+            <span onclick="deleteTask('${task['taskId']}')"> Delete </span>
           </div>
           <div class="popup-del-edit">
             <img src="/assets/img/pen_DARK.png">
-            <span onclick="editTask('${task['taskId']}')"> Edit </span>
+            <span onclick="editPopupTask('${task['taskId']}'); showPopupEditContainer(); closePopup();"> Edit </span>
           </div>
         </div>
       </div>
@@ -73,6 +73,277 @@ function openTaskDetails(taskId) {
       document.getElementById('popup-task-container').classList.add('animate-popup-task-container');
     }, 200);
   }
+  
+}
+
+function loadContactListPopup() {
+  let database = firebase.database();
+  let contactEntries = database.ref('contacts');
+
+  contactEntries.on('value', function(snapshot) {
+      // let contactsList = document.getElementById('contacts-list-popup-detail');
+      // contactsList.innerHTML = "";
+
+      allContacts = [];
+      snapshot.forEach(function(childSnapshot) {
+          let contact = childSnapshot.val();
+          allContacts.push(contact);
+
+          // contactsList.innerHTML += /*html*/ `
+          //     <div class="dropdown-item">
+          //       <div style="display: flex; align-items:center; justify-content: center; background: ${contact['color']}; height: 40px; width: 40px; border-radius: 40px; cursor: pointer;" id="contact-badge-list">${getInitials(contact['name'])}</div>
+          //       <label for="${contact['email']}">${contact['name']}</label>
+          //       <input type="checkbox" id="${contact['email']}" value="${contact['email']}" onclick="toggleContactAssignment(this)">
+          //     </div>`;
+      });
+  });
+}
+
+function editPopupTask(taskId) {
+  
+  let task = allTasks.find(t => t.taskId === taskId);
+
+  if (task) {
+    
+    let editTask = document.getElementById('task-popup-edit-container');
+    let currentTitle, currentDescription, currentDate, currentSubTask, currentCategory, currentPriority, currentPriorityText, currentSelection, currentCategoryColor;
+  
+    for (let i = 0; i < allTasks.length; i++) {
+      if (taskId === allTasks[i]['taskId']) {
+        currentTitle = allTasks[i]['taskTitle'];
+        currentDescription = allTasks[i]['taskDescription'];
+        currentDate = allTasks[i]['taskDate'];
+        currentSubTask = allTasks[i]['taskSubTask'] || [];
+        currentCategory = allTasks[i]['taskCategory'] || '';
+        currentCategoryColor = allTasks[i]['taskCategoryColor'];
+        currentPriority = allTasks[i]['taskPriority'] || '';
+        currentPriorityText = allTasks[i]['taskPriorityText'] || '';
+        currentSelection = allTasks[i]['taskAssignment'] || [];
+        break;
+      }
+    }
+  
+    let contactsList = '';
+    allContacts.forEach(contact => {
+      let isChecked = currentSelection.some(selectedContact => selectedContact.email === contact.email);
+      contactsList += /*html*/ `
+        <div class="dropdown-item">
+          <div style="display: flex; align-items:center; justify-content: center; background: ${contact['color']}; height: 40px; width: 40px; border-radius: 40px; cursor: pointer;" id="contact-badge-list">${getInitials(contact['name'])}</div>
+          <label for="${contact['email']}">${contact['name']}</label>
+          <input type="checkbox" id="${contact['email']}" value="${contact['email']}" onclick="toggleContactEditPopup(this)" ${isChecked ? 'checked' : ''}>
+        </div>
+      `;
+    });
+  
+    editTask.innerHTML = /*html*/ `
+      <input type="text" id="edit-task-title-container-popup" value="${currentTitle}">
+      <input type="text" id="edit-task-description-container-popup" value="${currentDescription}">
+      <input type="date" id="edit-task-date-container-popup" value="${currentDate}">
+      <input type="text" id="edit-task-subtask-input-popup" placeholder="Subtask">
+      <button type="button" onclick="addSubTaskEditPopup()">Add Subtask</button>
+      <ul id="subtask-list-edit-popup"></ul>
+      <div id="edit-task-category-container-popup">
+        <div class="dropbtn-popup" id="categorydropbtn-popup" style="background: ${currentCategoryColor}">${currentCategory}</div>
+        <div id="categoryDropdown" >
+          <a href="#" onclick="selectCategoryEditPopup('Technical Task')">Technical Task</a>
+          <a href="#" onclick="selectCategoryEditPopup('User Story')">User Story</a>
+        </div>
+      </div>
+      <div id="edit-task-priority-container-popup">
+        <div class="dropbtn-popup">${currentPriorityText} <img src="${currentPriority}" alt="Priority Icon"/></div>
+        <div id="priorityDropdown-popup" >
+          <a href="#" onclick="selectPriorityEditPopup('/assets/img/urgent_icon.svg', 'Urgent')">Urgent</a>
+          <a href="#" onclick="selectPriorityEditPopup('/assets/img/medium_icon.svg', 'Medium')">Medium</a>
+          <a href="#" onclick="selectPriorityEditPopup('/assets/img/low_icon.svg', 'Low')">Low</a>
+        </div>
+      </div>
+      <section id="edit-task-assignment-container-popup">
+          <div id="contact-list-selection-popup" onclick="toggleContactDropdownPopup()">
+            <span>Assign Contacts</span>
+            <div class="icon-dropdown-arrow">
+                <img id="icon-contacts-dropdown-arrow-image-popup" src="/assets/img/dropdown_down.svg" alt="Dropdown Arrow Icon"/>
+            </div>
+          </div>  
+          <div id="contacts-list-popup-detail" class="dropdown-list d-none">
+            ${contactsList}
+          </div>
+        
+      </section>
+      <button onclick="saveEditedPopupTask('${taskId}'); openTaskDetails('${taskId}'); hidePopupEditContainer()">Save</button>
+    `;
+  
+    selectedContactsEdit = currentSelection.slice();
+    subtasksEdit = currentSubTask.slice();
+    displaySubTasksEditPopup();
+    loadContactListPopup();
+  }
+}
+
+function toggleContactsDropdown() {
+  const dropdown = document.getElementById('contactsDropdown');
+  dropdown.classList.toggle('show');
+}
+
+function showPopupEditContainer() {
+  document.getElementById('task-popup-edit-container').style.display = 'flex';
+}
+
+function hidePopupEditContainer() {
+  document.getElementById('task-popup-edit-container').style.display = 'none';
+}
+
+
+function toggleContactEditPopup(checkbox) {
+  let contactEmail = checkbox.value;
+  if (checkbox.checked) {
+    let contact = allContacts.find(contact => contact.email === contactEmail);
+    selectedContactsEdit.push(contact);
+  } else {
+    selectedContactsEdit = selectedContactsEdit.filter(contact => contact.email !== contactEmail);
+  }
+}
+
+function toggleContactAssignmentPopup(checkbox) {
+  let contactEmail = checkbox.value;
+  if (checkbox.checked) {
+    let contact = allContacts.find(contact => contact['email'] === contactEmail);
+    selectedContacts.push(contact);
+  } else {
+    selectedContacts = selectedContacts.filter(contact => contact['email'] !== contactEmail);
+  }
+}
+
+
+function toggleContactDropdownPopup() {
+  let contactsDropdown = document.getElementById('contacts-list-popup-detail');
+  let dropdownArrow = document.getElementById('icon-contacts-dropdown-arrow-image-popup');
+
+  if (contactsDropdown.classList.contains('d-none')) {
+      contactsDropdown.classList.remove('d-none');
+      dropdownArrow.src = "/assets/img/dropdown_up.svg";
+  } else {
+      contactsDropdown.classList.add('d-none');
+      dropdownArrow.src = "/assets/img/dropdown_down.svg";
+  }
+}
+
+
+function addSubTaskEditPopup() {
+  let subTaskInputEdit = document.getElementById('edit-task-subtask-input-popup');
+  let subTaskValueEdit = subTaskInputEdit.value.trim();
+
+  if (subTaskValueEdit !== "") {
+    let subTask = {
+      name: subTaskValueEdit,
+      done: false
+    };
+    subtasksEdit.push(subTask);
+    subTaskInputEdit.value = "";
+    displaySubTasksEditPopup();
+  }
+}
+
+
+function displaySubTasksEditPopup() {
+  let subtaskListEdit = document.getElementById('subtask-list-edit-popup');
+  subtaskListEdit.innerHTML = "";
+
+  subtasksEdit.forEach((subtask, index) => {
+    let subtaskItemEdit = document.createElement('li');
+    subtaskItemEdit.innerHTML = /*html*/`
+      <input type="checkbox" id="subtask-edit-${index}" ${subtask.done ? 'checked' : ''} onclick="toggleSubtaskDoneEdit(${index})">
+      <label for="subtask-edit-${index}">${subtask.name}</label>
+      <button type="button" onclick="removeSubTaskEditPopup(${index})">X</button>
+    `;
+    subtaskListEdit.appendChild(subtaskItemEdit);
+  });
+}
+
+
+function removeSubTaskEditPopup(index) {
+  subtasksEdit.splice(index, 1);
+  displaySubTasksEditPopup();
+}
+
+function toggleCategoryDropdownPopup() {
+  let categoryDropdown = document.getElementById('task-category-dropdown');
+  let dropdownArrow = document.getElementById('icon-dropdown-arrow-image');
+
+  if (categoryDropdown.classList.contains('d-none')) {
+      categoryDropdown.classList.remove('d-none');
+      dropdownArrow.src = "/assets/img/dropdown_up.svg";
+  } else {
+      categoryDropdown.classList.add('d-none');
+      dropdownArrow.src = "/assets/img/dropdown_down.svg";
+  }
+}
+
+
+function selectCategoryPopup(event, category) {
+  selectedCategory = category;
+  document.getElementById('task-category-selection').innerText = selectedCategory;
+  document.getElementById('task-category-dropdown').classList.add('d-none');
+  document.getElementById('icon-dropdown-arrow-image').src = "/assets/img/dropdown_down.svg";
+}
+
+function selectCategoryEditPopup(category) {
+  let categoryColor;
+  if (category === 'User Story') {
+    categoryColor = '#0038ff';
+  } else if (category === 'Technical Task') {
+    categoryColor = '#1fd7c1';
+  }
+
+  document.getElementById('categorydropbtn-popup').innerText = category;
+  document.getElementById('categorydropbtn-popup').style.background = categoryColor;
+}
+
+
+function selectPriorityEditPopup(priorityIcon) {
+  document.querySelector('#edit-task-priority-container-popup .dropbtn-popup img').src = priorityIcon;
+}
+
+
+function saveEditedPopupTask(taskId) {
+  
+  let editTitle = document.getElementById('edit-task-title-container-popup').value;
+  let editDescription = document.getElementById('edit-task-description-container-popup').value;
+  let editDate = document.getElementById('edit-task-date-container-popup').value;
+  let editCategory = document.querySelector('#edit-task-category-container-popup .dropbtn-popup').innerText;
+
+  let editCategoryColor;
+  if (editCategory === 'User Story') {
+    editCategoryColor = '#0038ff';
+  } else if (editCategory === 'Technical Task') {
+    editCategoryColor = '#1fd7c1';
+  } else {
+    editCategoryColor = '#ffffff';
+  }
+
+  let editPriority = document.querySelector('#edit-task-priority-container-popup .dropbtn-popup img').src;
+  let editPriorityText = document.querySelector('#edit-task-priority-container-popup .dropbtn-popup').innerText.split(' ')[0];
+
+  let task = {
+    taskTitle: editTitle,
+    taskDescription: editDescription,
+    taskDate: editDate,
+    taskSubTask: subtasksEdit,
+    taskCategory: editCategory,
+    taskCategoryColor: editCategoryColor,
+    taskPriority: editPriority,
+    taskPriorityText: editPriorityText,
+    taskAssignment: selectedContactsEdit,
+    taskBoardCategory: boardCategory[0]
+  };
+
+  let database = firebase.database();
+  let editedTask = database.ref('tasks').child(taskId);
+  editedTask.update(task);
+
+  document.getElementById('task-popup-edit-container').innerHTML = "";
+  selectedContactsEdit = [];
+  subtasksEdit = [];
+  
 }
 
 
@@ -83,6 +354,18 @@ function toggleSubtaskDoneInTask(taskId, subtaskIndex) {
     subtask.done = !subtask.done;
     taskRef.set(subtask);
   });
+}
+
+
+function addSubtaskField() {
+  const subtasksContainer = document.getElementById('edit-subtasks');
+  const subtaskIndex = subtasksContainer.children.length;
+  const newSubtaskHTML = /*html*/ `
+    <div class="subtask">
+      <input type="checkbox" id="edit-subtask-${subtaskIndex}">
+      <input type="text" id="edit-subtask-name-${subtaskIndex}" placeholder="New Subtask">
+    </div>`;
+  subtasksContainer.insertAdjacentHTML('beforeend', newSubtaskHTML);
 }
 
 
@@ -116,6 +399,7 @@ function filterTask() {
 
 
 window.onload = function() {
+  loadContactListPopup();
     loadTasksBoard();
     includeHTML();
 }
